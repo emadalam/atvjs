6c1d029f1f3c71b9f688fc6ac2fdefa6ef1e1e9c
Sergey Shoshin <sergey.shoshin@vmedia.ca>
2017-01-17 11:28:40 +0300

    dist folder sync with latest changes (options & custom menu)

--- dist/atv.js
+++ dist/atv.js
@@ -98,7 +98,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _settings2 = _interopRequireDefault(_settings);
 
-	var _menu = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./menu\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var _menu = __webpack_require__(307);
 
 	var _menu2 = _interopRequireDefault(_menu);
 
@@ -22131,7 +22131,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _navigation2 = _interopRequireDefault(_navigation);
 
-	var _menu = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./menu\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var _menu = __webpack_require__(307);
 
 	var _menu2 = _interopRequireDefault(_menu);
 
@@ -22519,7 +22519,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _parser2 = _interopRequireDefault(_parser);
 
-	var _menu = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./menu\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var _menu = __webpack_require__(307);
 
 	var _menu2 = _interopRequireDefault(_menu);
 
@@ -23064,7 +23064,210 @@ return /******/ (function(modules) { // webpackBootstrap
 	};
 
 /***/ },
-/* 307 */,
+/* 307 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _lodash = __webpack_require__(298);
+
+	var _lodash2 = _interopRequireDefault(_lodash);
+
+	var _parser = __webpack_require__(302);
+
+	var _parser2 = _interopRequireDefault(_parser);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	// base menu string for initial document creation
+	var docStr = '<document><menuBarTemplate><menuBar></menuBar></menuBarTemplate></document>';
+	// indicate whether the menu was created
+	var created = false;
+
+	// few private instances
+	var doc = void 0;
+	var menuBarEl = void 0;
+	var menuBarFeature = void 0;
+	var itemsCache = {};
+
+	// default menu options
+	var defaults = {
+	    attributes: {},
+	    items: []
+	};
+
+	/**
+	 * Sets the default menu options
+	 *
+	 * @param {Object} cfg The configuration object {defaults}
+	 */
+	function setOptions() {
+	    var cfg = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+	    console.log('setting menu options...', cfg);
+	    // override the default options
+	    _lodash2.default.assign(defaults, cfg);
+	}
+
+	/**
+	 * Iterates and sets attributes to an element.
+	 * 
+	 * @param {Element} el 			The element to set attributes on
+	 * @param {Object} attributes 	Attributes key value pairs.
+	 */
+	function setAttributes(el, attributes) {
+	    console.log('setting attributes on element...', el, attributes);
+	    _lodash2.default.each(attributes, function (value, name) {
+	        return el.setAttribute(name, value);
+	    });
+	}
+
+	/**
+	 * Returns instance of the menu document (auto create if not already created)
+	 * 
+	 * @return {Promise}		Promise with instance of the created menu document.
+	 */
+	function get() {
+	    if (!created) {
+	        return create();
+	    }
+	    return Promise.resolve(doc);
+	}
+
+	/**
+	 * Adds menu item to the menu document.
+	 * 
+	 * @param {Object} item 	The configuration realted to the menu item.
+	 */
+	function addItem() {
+	    var item = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+	    if (!item.id) {
+	        console.warn('Cannot add menuitem. A unique identifier is required for the menuitem to work correctly.');
+	        return;
+	    }
+	    var el = doc.createElement('menuItem');
+	    // assign unique id
+	    item.attributes = _lodash2.default.assign({}, item.attributes, {
+	        id: item.id
+	    });
+	    // add all attributes
+	    setAttributes(el, item.attributes);
+	    // add title
+	    el.innerHTML = '<title>' + (_lodash2.default.isFunction(item.name) ? item.name() : item.name) + '</title>';
+	    // add page reference
+	    el.page = item.page;
+	    // add page options reference
+	    el.options = item.options;
+	    // appends to the menu
+	    menuBarEl.insertBefore(el, null);
+	    // cache for later use
+	    itemsCache[item.id] = el;
+
+	    return el;
+	}
+
+	/**
+	 * Generates a menu from the configuration object.
+	 * 
+	 * @param  {Object} cfg 		Menu related configurations
+	 * @return {Promise<Document>}     		Promise with created menu document
+	 */
+	function create() {
+	    var cfg = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+	    if (created) {
+	        console.warn('An instance of menu already exists, skipping creation...');
+	        return Promise.resolve(doc);
+	    }
+	    // defaults
+	    _lodash2.default.assign(defaults, cfg);
+
+	    console.log('creating menu...', defaults);
+
+	    var afterMenuDocCreated = function afterMenuDocCreated(res) {
+	        doc = res;
+	        menuBarEl = doc.getElementsByTagName('menuBar').item(0);
+	        menuBarFeature = menuBarEl && menuBarEl.getFeature('MenuBarDocument');
+	        // set attributes to the menubar element
+	        setAttributes(menuBarEl, defaults.attributes);
+	        // add all items to the menubar
+	        _lodash2.default.each(defaults.items, function (item) {
+	            return addItem(item);
+	        });
+	        // indicate done
+	        created = true;
+	        return doc;
+	    };
+
+	    if (!!defaults.template) {
+	        // if template was specified, use it.
+	        docStr = _lodash2.default.isFunction(defaults.template) ? defaults.template() : defaults.template;
+	    }
+
+	    return _parser2.default.dom(docStr).then(afterMenuDocCreated);
+	}
+
+	/**
+	 * Associate a document to the menuitem (using the menuitem's unique id).
+	 * 
+	 * @param {Document} doc        	The document to associate with the menuitem
+	 * @param {String} menuItemid		The id of the menu item as per the configuration
+	 */
+	function setDocument(doc, menuItemid) {
+	    var menuItem = itemsCache[menuItemid];
+
+	    if (!menuItem) {
+	        console.warn('Cannot set document to the menuitem. The given id ' + menuItemid + ' does not exist.');
+	        return;
+	    }
+	    menuBarFeature.setDocument(doc, menuItem);
+	}
+
+	/**
+	 * Set the given menuitem as active (using the menuitem's unique id).
+	 * 
+	 * @param {String} menuItemid 		The id of the menu item as per the configuration
+	 */
+	function setSelectedItem(menuItemid) {
+	    var menuItem = itemsCache[menuItemid];
+
+	    if (!menuItem) {
+	        console.warn('Cannot select menuitem. The given id ' + menuItemid + ' does not exist.');
+	        return;
+	    }
+	    menuBarFeature.setSelectedItem(menuItem);
+	}
+
+	/**
+	 * A very minimalistic library to manage Apple TV menu bars.
+	 *
+	 * @author eMAD <emad.alam@yahoo.com>
+	 *
+	 */
+	exports.default = {
+	    get created() {
+	        return created;
+	    },
+	    set created(val) {},
+	    setOptions: setOptions,
+	    create: create,
+	    get: get,
+	    setDocument: setDocument,
+	    setSelectedItem: setSelectedItem,
+	    getLoadingMessage: function getLoadingMessage() {
+	        return _lodash2.default.isFunction(defaults.loadingMessage) ? defaults.loadingMessage() : defaults.loadingMessage;
+	    },
+	    getErrorMessage: function getErrorMessage() {
+	        return _lodash2.default.isFunction(defaults.errorMessage) ? defaults.errorMessage() : defaults.errorMessage;
+	    }
+	};
+
+/***/ },
 /* 308 */
 /***/ function(module, exports, __webpack_require__) {
 
